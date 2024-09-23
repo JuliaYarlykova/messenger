a1e20f767e121b030c19d48130da9aa8a418aa4b
juliayarlykova <yarlykova2506@gmail.com>
2024-09-23 16:47:50 +0700

    Task4: подготовила компонент к работе с вебсокетом

--- frontend/src/app/types/global.d.ts
+++ frontend/src/app/types/global.d.ts
@@ -1,24 +1,25 @@
 declare const __IS_DEV__: boolean
 declare const __API__: string
+declare const __SOCKETURL__: string
 
 declare module '*.scss' {
-	type IClassNames = Record<string, string>
-	const classNames: IClassNames
-	export = classNames
+  type IClassNames = Record<string, string>
+  const classNames: IClassNames
+  export = classNames
 }
 
 declare module '*.png'
 declare module '*.jpg'
 declare module '*.jpeg'
 declare module '*.svg' {
-	import type React from 'react'
+  import type React from 'react'
 
-	const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
-	// @ts-ignore
-	export default SVG
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  // @ts-ignore
+  export default SVG
 }
 /// <reference types="vite/client" />
 
 type OptionalRecord<K extends keyof any, T> = {
-	[P in K]?: T
+  [P in K]?: T
 }
---
+++ frontend/src/features/Chating/index.ts
@@ -0,0 +1,1 @@
+export { ChatPart } from './ui/ChatPart'
